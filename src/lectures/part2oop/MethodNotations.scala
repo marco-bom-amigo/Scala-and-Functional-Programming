df7c4e25a109c9d5f2114b49d5aedaf42b10c2d8
M. Bonamichi <marco.bom.amigo@gmail.com>
2018-06-30 12:21:15 -0300

    Method Notations Exercies.

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,14 +2,24 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
 
     def +(person: Person):String = s"${this.name} is friend of ${person.name}"
+    def +(nickname:String): Person = new Person(name = s"$name ($nickname)", favoriteMovie = favoriteMovie)
+
     def unary_! : String = s"$name what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+
     def isAlive: Boolean = true
+
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie!!"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times...!"
+
+    def learns(thing: String): String = s"$name is learning $thing"
+    def learnsScala: String = this learns "Scala"
+
   }
 
   val mary = new Person(name = "Mary", favoriteMovie = "Inception")
@@ -34,5 +44,22 @@ object MethodNotations extends App {
 
   println(mary.apply())
   println(mary())
+
+  /*
+  * 1. Overload the + operator, which receive an string and return a person with a nickname, between parenthesis
+  *   mary + "Nickname"
+  * 2. Add an age to the person class, and add an unary operator, which increment age
+  *   +mary
+  * 3. Add a "learns" method
+  *     mary learns Scala
+  *    Add a learnsScala method, calls the learns method, with "Scala"
+  * 4. Overload the Apply method
+  *     mary(2) "Mary watched Inception 2 times."
+  * */
+
+  println((mary + "the Rockstar")())
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(8))
 
 }
